40d2277adf7357784c0b14a57abb07fe92db28f7
Gabriel Costa <22gabrielgc@gmail.com>
2023-05-17 18:02:38 -0300

    Configurado Layout da pag de informações

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -7,13 +7,34 @@ const Body = styled.div`
   background-color: ${(props) => props.theme.colors.bggrey};
   min-height: 100vh;
 `
+const Content = styled.div`
+    padding: 80px 0;
+`
+
+const Text = styled.p`
+    font-size: 20px;
+    padding: 0;
+    margin: 0;
+`
+
+const CityName = styled.h2`
+    font-size: 60px;
+    padding: 0;
+    margin: 0;
+`
 
 export default function InfoPage() {
   return (
     <>
       <Navbar />
       <Body>
-        <Container>Batata2</Container>
+        <Container> 
+            <Content>
+                <Text>Previsão do tempo para</Text>
+                <CityName>Lavras, MG, Brasil</CityName>
+            </Content>
+            
+        </Container>
       </Body>
     </>
   )
