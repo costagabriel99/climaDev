ea4677f9f47a84973bae773cbc8d901f6e4ada7b
Gabriel Costa <22gabrielgc@gmail.com>
2023-05-23 10:53:24 -0300

    Corrigido https na url da api e trocado chave api

--- pages/api/openweathermap/index.js
+++ pages/api/openweathermap/index.js
@@ -1,6 +1,6 @@
 export default async function handle(req, res) {
   const url = decodeURIComponent(req.url).replace('/api/openweathermap?url=', '')
-  const response = await fetch(`http://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
+  const response = await fetch(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
   const json = await response.json()
   res.status(response.status).json(json)
 }
